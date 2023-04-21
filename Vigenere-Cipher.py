1bbe29dc9fb99eb44bb074e39a7c8b56ae01dd7e
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 10:06:30 +0800

    Psuedocode

---
+++ Vigenere-Cipher.py
@@ -0,0 +1,6 @@
+#Kunin yung input ni User AKA yung Message na gustong encrypt ni User
+#kunin yung Input ni user na keyword na gusto niyang gamitin
+#lalgyan mo nung cyhered text
+#Turn the characters of the User input to the respective number values ranging from 0-25
+#for loop para sa encryption
+#print mo na output mo
