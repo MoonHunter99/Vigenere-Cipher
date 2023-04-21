9def0291457939fbf16e3c9fd2a9f43621516989
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 10:20:44 +0800

    did a ord function in which i sturggled to do when i started to code this program for the first time. this program is finished and working but i will still do some designs so it looks good

--- Vigenere-Cipher.py
+++ Vigenere-Cipher.py
@@ -3,6 +3,13 @@ user_input_message = str(input("What is your message that you want to encrypt?: 
 #kunin yung Input ni user na keyword na gusto niyang gamitin
 user_input_keyword = str(input("What is the Keyword that you would like to use?: "))
 #lalgyan mo nung cyhered text
+chpr_txt = ""
 #Turn the characters of the User input to the respective number values ranging from 0-25
+number_value_msg = [ord(w)-65 for w in user_input_message]
+number_value_key = [ord(w)-65 for w in user_input_keyword]
 #for loop para sa encryption
+for i in range(len(user_input_message)):
+    number_value = (number_value_msg[i] + number_value_key[i % len(user_input_keyword)]) % 26
+    chpr_txt += chr(number_value + 65)
 #print mo na output mo
+print(chpr_txt)
