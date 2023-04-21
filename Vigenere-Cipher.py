8a191c264faa34294c946c209913490051cb82c4
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 10:12:31 +0800

    User input(keyword) working(last commit is the message to be encrypted)

--- Vigenere-Cipher.py
+++ Vigenere-Cipher.py
@@ -1,6 +1,7 @@
 #Kunin yung input ni User AKA yung Message na gustong encrypt ni User
 user_input_message = str(input("What is your message that you want to encrypt?: "))
 #kunin yung Input ni user na keyword na gusto niyang gamitin
+user_input_keyword = str(input("What is the Keyword that you would like to use?: "))
 #lalgyan mo nung cyhered text
 #Turn the characters of the User input to the respective number values ranging from 0-25
 #for loop para sa encryption
