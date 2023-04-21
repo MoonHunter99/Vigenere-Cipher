50669d79c2c25066685ab7c11b809a1000c7b0eb
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 10:08:46 +0800

    user input working

--- Vigenere-Cipher.py
+++ Vigenere-Cipher.py
@@ -1,4 +1,5 @@
 #Kunin yung input ni User AKA yung Message na gustong encrypt ni User
+user_input_message = str(input("What is your message that you want to encrypt?: "))
 #kunin yung Input ni user na keyword na gusto niyang gamitin
 #lalgyan mo nung cyhered text
 #Turn the characters of the User input to the respective number values ranging from 0-25
